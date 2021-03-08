71d8c1cfcea2cecc6fc3c3d5c8674774f29fd2ef
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-08 17:37:03 +0900

    refactor 제목수정

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          My Memories
+        <Typography className={classes.heading} variant="h4" align="center">
+          Memories..
         </Typography>
         <img
           className={classes.image}
