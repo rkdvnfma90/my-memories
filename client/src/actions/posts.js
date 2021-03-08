6f4698d2253f84f50f0b98eef42c8a195070c776
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-08 18:04:41 +0900

    refactor 상수관리

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL, UPDATE, CREATE, DELETE } from '../constants/actionTypes'
 import * as api from '../api'
 
 // 액션 생성자 (redux thunk 사용하기 위함)
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts() // response.data
-    dispatch({ type: 'FETCH_ALL', payload: data })
+    dispatch({ type: FETCH_ALL, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -13,7 +14,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post)
-    dispatch({ type: 'CREATE', payload: data })
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -23,7 +24,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
 
-    dispatch({ type: 'UPDATE', payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -32,7 +33,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id)
-    dispatch({ type: 'DELETE', payload: id })
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
     console.log(error)
   }
@@ -42,7 +43,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id)
 
-    dispatch({ type: 'UPDATE', payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error)
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,17 @@
+import { FETCH_ALL, UPDATE, CREATE, DELETE } from '../constants/actionTypes'
+
 const posts = (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload
-    case 'CREATE':
+    case CREATE:
       return [...posts, action.payload]
-    case 'LIKE':
-    case 'UPDATE':
+    case UPDATE:
       // actions.payload 에는 업데이트된 post가 담겨있음
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       )
-    case 'DELETE':
+    case DELETE:
       // action.payload 에 삭제된 id 값이 들어있음
       return posts.filter((post) => post._id !== action.payload)
     default:
