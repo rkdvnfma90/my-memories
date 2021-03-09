6fdd9c5b5dee1b29f1843d5640c0248a54092b23
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-03-09 10:05:37 +0900

    feat 로그인,회원인증 페이지 작성

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import useStyles from './styles'
+import Input from './Input'
 
 function Auth() {
-  const state = null
-  return <div>AUTH</div>
+  const classes = useStyles()
+  const [showPassword, setShowPassword] = useState(false)
+  const [isSignup, setIsSignup] = useState(false)
+
+  const handleSubmit = () => {}
+  const handleChange = () => {}
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword)
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup)
+    setShowPassword(false)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? '회원가입' : '로그인'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignup ? '회원가입' : '로그인'}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? '계정이 이미 있으시면 로그인 해주세요.'
+                  : '계정이 없으시면 회원가입 해주세요.'}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  )
 }
 
 export default Auth
